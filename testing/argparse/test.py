461ce6729cc68aa743953f58bb9d304dd9c0a284
jifwin <gpietrusza@gmail.com>
2015-02-09 15:58:18 +0100

    3.3

--- testing/argparse/test.py
+++ testing/argparse/test.py
@@ -1,5 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename")
-parser.parse_args()
+parser.add_argument("--filename")
+parser.add_argument("--pixels",default=5)
+arguments = parser.parse_args()
+print arguments.filename
+print arguments.pixels
